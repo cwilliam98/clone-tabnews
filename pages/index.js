49a84ebc705928aa03ab3f51b254d1a367a411ed
Cristian Albrecht <cristianwillian174@gmail.com>
2023-05-19 10:21:39 +0000

    Modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home(){
     return <h1>Di, você não está atrasada em sua vida profissional, 
                 só precisa de uma boa oportunidade para se destacar. Saiba que sempre vou te apoiar!!
 
-                E lembre-se, olha para o que você tem em mãos e use isso!
+                E lembre-se, olha para o que você tem em mãos e use isso! :)
             </h1>
 }
 
