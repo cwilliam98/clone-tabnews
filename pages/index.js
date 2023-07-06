6c88ebd3252f491ac40d3b02413a7dd244fdd47b
Cristian Albrecht <cristianwillian174@gmail.com>
2023-07-06 00:32:50 +0000

    adiciona scripts `lint:check e lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-function Home(){
-    return <h1>Di, você não está atrasada em sua vida profissional, 
-                só precisa de uma boa oportunidade para se destacar. Saiba que sempre vou te apoiar!!
-
-                E lembre-se, olha para o que você tem em mãos e use isso! :)
-            </h1>
+function Home() {
+  return (
+    <h1>
+      Di, você não está atrasada em sua vida profissional, só precisa de uma boa
+      oportunidade para se destacar. Saiba que sempre vou te apoiar!! E
+      lembre-se, olha para o que você tem em mãos e use isso! :)
+    </h1>
+  );
 }
 
 export default Home;
