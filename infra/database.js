20084dab56598fd79be357ed57311e0c467e2573
Cristian Albrecht <cristianwillian174@gmail.com>
2025-02-01 19:24:23 +0000

    makes   more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -13,13 +13,24 @@ function getClient() {
 
 async function query(queryObject) {
   const client = getClient();
-  await client.connect();
+
+  console.log("Credencias do Postgress:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
 
   try {
+    await client.connect();
+
     const result = await client.query(queryObject);
+
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
